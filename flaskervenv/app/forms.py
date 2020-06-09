ba5067dfe99105e56ef664152afcece4e5c1cb67
Robert Hubert <robertfhubert@gmail.com>
2020-06-08 20:18:23 -0400

    add about me

--- flaskervenv/app/forms.py
+++ flaskervenv/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -26,4 +26,10 @@ class RegistrationForm(FlaskForm):
         email = User.query.filter_by(email=email.data).first()
         if email is not None:
             raise ValidationError('There is already an account registered to that Email')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = StringField('About Me', validators=[Length(min=0, max=140)])
+    submit= SubmitField('Submit')
+
  
